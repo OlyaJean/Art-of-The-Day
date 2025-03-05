f584a9477bc9be3c22d0b74a3e6caf03c7eea007
OlyaJean <olga_lavida@yahoo.com>
2025-03-05 11:39:37 -0500

    fetched random art

--- src/main.js
+++ src/main.js
@@ -2,8 +2,23 @@ import './style.css'
 
 const img = document.getElementById('image');
 
+const artApiUrl = 'https://api.artic.edu/api/v1/artworks?limit=50'
+// Make a GET request using the Fetch API
+fetch(artApiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(artData => {
+   let allArt = artData.data;
+   let randomArtIndex = Math.floor(Math.random() * (allArt.length - 1))
 
-fetch('https://api.artic.edu/api/v1/artworks')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+    console.log('Art Data:', allArt[randomArtIndex]);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+  
