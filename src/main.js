ee688a962ac43686fab7f89b916b72c99bc3bab2
OlyaJean <olga_lavida@yahoo.com>
2025-03-05 20:55:52 -0500

    title,artist,description

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import './style.css'
 
 const img = document.getElementById('image');
+const artTitle = document.getElementById('artTitle');
+const artistName = document.getElementById('artistName');
+const artDescription = document.getElementById('artDescription');
 
+//Return only 50 artworks 
 const artApiUrl = 'https://api.artic.edu/api/v1/artworks?limit=50'
-// Make a GET request using the Fetch API
+
 fetch(artApiUrl)
   .then(response => {
     if (!response.ok) {
@@ -13,9 +17,25 @@ fetch(artApiUrl)
   })
   .then(artData => {
    let allArt = artData.data;
-   let randomArtIndex = Math.floor(Math.random() * (allArt.length - 1))
+   let randomArtIndex = Math.floor(Math.random() * (allArt.length - 1));
+   let artForDisplay = allArt[randomArtIndex]
+   //get title
+   artTitle.textContent = artForDisplay.title;
 
-    console.log('Art Data:', allArt[randomArtIndex]);
+   //get artist name
+   artistName.textContent = artForDisplay.artist_title;
+
+   //get art description
+   if(!artForDisplay.description){
+    artDescription.textContent = 'Description is not available.'
+   }else{
+    artDescription.textContent = artForDisplay.description
+   }
+   //get image
+   let imgSrc = `https://www.artic.edu/iiif/2/${artForDisplay.image_id}/full/843,/0/default.jpg`;
+   img.src = imgSrc;
+    console.log('Art Data:', artForDisplay);
+    console.log(imgSrc);
   })
   .catch(error => {
     console.error('Error:', error);
